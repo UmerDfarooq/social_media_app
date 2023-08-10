43b73643147302d3ff7b14a9773f470baa767101
aamirsatti-as <aamirsatti507@gmail.com>
2023-08-11 01:26:53 +0500

    Sidebar completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import { ModeToggle } from "../components/ui/dark-mode";
-
+import Sidebar from "../components/sidebar/page";
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24"></main>
+    <main className="flex flex-col items-center justify-between p-24">
+      {/* <Sidebar /> */}
+      Children Content
+    </main>
   );
 }
